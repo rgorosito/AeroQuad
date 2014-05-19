83f6111cc8b6a161db9a079693b14462ea8e1735
Ricardo A. Gorosito <rgorosito@gmail.com>
2014-05-18 21:25:43 -0300

    Uso el sfu-util del S.O. (Fedora 20)

--- sube.sh
+++ sube.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #subir
-sudo /home/rgorosito/git/dfu-util/src/dfu-util --reset --device 0483:df11 --alt 0 --dfuse-address 0x08010000 --download /home/rgorosito/git/AeroQuad/BuildAQ32/objSTM32/AeroQuad32/AeroQuadMain.bin
+sudo dfu-util --reset --device 0483:df11 --alt 0 --dfuse-address 0x08010000 --download /home/rgorosito/git/AeroQuad/BuildAQ32/objSTM32/AeroQuad32/AeroQuadMain.bin
 
 
 
